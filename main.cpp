5c6a22d4afbc317b843bdf0ff39f7a4a06b418a8
Stephen <stwhetstone@gmail.com>
2025-04-12 04:05:45 -0400

    deltaTime and flock

--- main.cpp
+++ main.cpp
@@ -24,19 +24,24 @@ class Boid {
 			this->accel = accel;
 		}
 
-		void update() {
-			this->pos.x += this->vel.x;
-			this->pos.y += this->vel.y;
+		void update(float deltaTime) {
+			this->pos.x += this->vel.x * deltaTime;
+			this->pos.y += this->vel.y * deltaTime;
 
-			this->vel.x += this->accel.x;
-			this->vel.y += this->accel.y;
+			this->vel.x += this->accel.x * deltaTime;
+			this->vel.y += this->accel.y * deltaTime;
 
-			if(this->pos.x + 20 >= WINDOW_WIDTH || this->pos.x - 20 < 0) {
+			if(this->pos.x + 20 >= WINDOW_WIDTH - 1) {
+				this->pos.x = WINDOW_WIDTH - 21;
+				this->vel.x *= -0.75f;
+			} else if (this->pos.x - 20 < 1) {
+				this->pos.x = 21;
 				this->vel.x *= -0.75f;
 			}
 
 			if(this->pos.y >= WINDOW_HEIGHT - 1) {
-				this->vel.y *= -0.95f;
+				this->pos.y = WINDOW_HEIGHT - 1;
+				this->vel.y *= -0.75f;
 			} 
 		}
 };
@@ -50,18 +55,36 @@ int main() {
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
 	SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-
 	if(!window || !renderer || !texture) {
 		return -1;
 	}
+	
+	SDL_SetRenderVSync(renderer, 1);
 
-
+	std::vector<Boid> flock;
+	
+	
+	flock.push_back(Boid(
+		(Vec2f){ WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 } ,
+		(Vec2f){ 150.0f, 0 }, 
+		(Vec2f){ 0, 500.0f }
+	));
+	flock.push_back(Boid(
+		(Vec2f){ WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 } ,
+		(Vec2f){ 100.0f, 0 }, 
+		(Vec2f){ 0, 500.0f }
+	));
+	flock.push_back(Boid(
+		(Vec2f){ WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 } ,
+		(Vec2f){ 200.0f, 0 }, 
+		(Vec2f){ 0, 500.0f }
+	));
+	
 	SDL_Event e;
 	bool quit = false;
-
-	Boid b((Vec2f){ WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 } ,
-			(Vec2f){ 0.05f, 0 }, 
-			(Vec2f){ 0, 0.003f });
+	
+	std::chrono::steady_clock::time_point curTime = std::chrono::steady_clock::now();
+	std::chrono::steady_clock::time_point prevTime = curTime;	
 
 	while(!quit) {
 		while(SDL_PollEvent(&e) != 0) {
@@ -69,24 +92,32 @@ int main() {
 				quit = true;
 			}
 		}
+
+		curTime = std::chrono::steady_clock::now();
+		std::chrono::duration<float> time = curTime - prevTime;
+		prevTime = curTime;
+
+		float deltaTime = time.count();
 		
 		SDL_SetRenderTarget(renderer, texture);
 		
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
 		
-		
 		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-		SDL_RenderLine(renderer, b.pos.x - 20, b.pos.y , b.pos.x + 20, b.pos.y);
-		SDL_RenderLine(renderer, b.pos.x - 20, b.pos.y , b.pos.x, b.pos.y - 34.641f);
-		SDL_RenderLine(renderer, b.pos.x + 20, b.pos.y , b.pos.x, b.pos.y - 34.641f);
+		for(auto &boid : flock) {
+			SDL_RenderLine(renderer, boid.pos.x - 20, boid.pos.y , boid.pos.x + 20, boid.pos.y);
+			SDL_RenderLine(renderer, boid.pos.x - 20, boid.pos.y , boid.pos.x, boid.pos.y - 34.641f);
+			SDL_RenderLine(renderer, boid.pos.x + 20, boid.pos.y , boid.pos.x, boid.pos.y - 34.641f);
+			
+			boid.update(deltaTime);
+		}
 		
+
 		SDL_SetRenderTarget(renderer, nullptr);
 		SDL_RenderTexture(renderer, texture, nullptr, nullptr);
 		
 		SDL_RenderPresent(renderer);
-
-		b.update();
 	}
 
 
